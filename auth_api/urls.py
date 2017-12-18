6f33f6f5629b8fa66b1cc318dca6360a20ac97c0
Pavel Pugin <pskrdx@gmail.com>
2017-12-19 00:26:02 +0100

    urls names

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -6,21 +6,21 @@ from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = [
-    url(r'^login/$', views.UserLoginView.as_view()),
-    url(r'^logout/$', views.UserLogoutView.as_view()),
-    url(r'^userdetails/$', views.UserDetailsView.as_view()),
-    url(r'^reset/$', views.PasswordResetInitView.as_view()),
-    url(r'^password/$', views.PasswordResetView.as_view()),
-    url(r'^invite/$', views.MakeInvitationLink.as_view()),
-    url(r'^register/$', views.RegisterView.as_view()),
-    url(r'^create_team/$', views.CreateTeamView.as_view()),
-    url(r'^verify_email/$', views.VerifyEmailView.as_view()),
-    url(r'^$', views.APIRoot.as_view()),
+    url(r'^login/$', views.UserLoginView.as_view(), name='login'),
+    url(r'^logout/$', views.UserLogoutView.as_view(), name='logout'),
+    url(r'^userdetails/$', views.UserDetailsView.as_view(), name='user details'),
+    url(r'^reset/$', views.PasswordResetInitView.as_view(), name='reset password'),
+    url(r'^password/$', views.PasswordResetView.as_view(), name = 'set password'),
+    url(r'^invite/$', views.MakeInvitationLink.as_view(), name='invite'),
+    url(r'^register/$', views.RegisterView.as_view(), name='register'),
+    url(r'^create_team/$', views.CreateTeamView.as_view(), name='create_team'),
+    url(r'^verify_email/$', views.VerifyEmailView.as_view(), name='verify_email'),
+    url(r'^$', views.APIRoot.as_view(), name='api root'),
 
     
 ]
 
 if settings.DEBUG:
     urlpatterns += [
-        url(r'^retrieve_code/$', views.RetrieveCodesView.as_view()), 
+        url(r'^retrieve_code/$', views.RetrieveCodesView.as_view(), name='get codes'), 
     ]
