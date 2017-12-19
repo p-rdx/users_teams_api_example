c5fc8b7f50b3029e0cac6a72edf369f628eac111
Pavel Pugin <pskrdx@gmail.com>
2017-12-19 12:41:15 +0100

    2nd bunch of tests, some reworking

--- auth_api/tests.py
+++ auth_api/tests.py
@@ -9,51 +9,6 @@ from django.core import mail
 
 from .models import CustomUser, Team, Membership, VerificationToken
 from rest_framework.authtoken.models import Token
-	
-
-class SuperUserTestCase(APITestCase):
-	su_email = 'super@user.com'
-	su_password = 'P2ssw0rd'
-	team_name = 'initial_team'
-	is_abstract = True
-		
-	@classmethod
-	def setUpClass(cls):
-		team = Team.objects.create(name=cls.team_name)
-		su = CustomUser.objects.create_superuser(email=cls.su_email, password=cls.su_password)
-		Membership.objects.create(user=su, team=team)
-
-	@classmethod
-	def tearDownClass(cls):
-		"""
-		this allows to have a superuser available for all other tests, maybe it is wrong exploration
-		"""
-		pass
-
-	def test_check_superuser(self):
-		su = CustomUser.objects.get(email=self.su_email)
-		team = Team.objects.get(name=self.team_name)
-		inv = Membership.objects.get(user=su, team=team)
-
-		self.assertTrue(su.email_verified)
-		self.assertFalse(VerificationToken.objects.filter(user=su).exists())
-		self.assertTrue(inv)
-
-	def test_login_and_logout(self):
-		url_login = reverse('login')
-		url_logout = reverse('logout')
-		user = CustomUser.objects.get(email=self.su_email)
-		
-		responce = self.client.post(url_login, {'email': self.su_email, 'password': self.su_password})
-		token = Token.objects.get(user=user)
-		self.assertEquals(responce.status_code, 200)
-		self.assertEquals(token.key, responce.data['key'])
-
-		self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
-		responce = self.client.post(url_logout)
-		token = Token.objects.filter(user=user).first()
-		self.assertEqual(responce.status_code, 200)
-		self.assertTrue(token is None)
 
 
 class UserRegistrationTestCase(APITestCase):
@@ -63,6 +18,15 @@ class UserRegistrationTestCase(APITestCase):
 		self.last_name = 'l'
 		self.password = 'P@ssw0rd'
 		credentials = {'email': self.email, 'first_name': self.first_name, 'last_name': self.last_name, 'password': self.password,}
+		team = Team.objects.create(name='team_name')
+		su = CustomUser.objects.create_superuser(email='super@...', password='password')
+		Membership.objects.create(user=su, team=team)
+
+	def tearDown(self):
+		Membership.objects.all().delete()
+		CustomUser.objects.all().delete()
+		Team.objects.all().delete()
+
 
 	def test_register_user(self): 
 		url = reverse('register')
@@ -72,8 +36,7 @@ class UserRegistrationTestCase(APITestCase):
 		self.assertEqual(responce.data['email'], self.email)
 		user = CustomUser.objects.get(email=self.email)
 		self.assertTrue(isinstance(user, CustomUser))
-		self.assertFalse(user.email_verified)
-		self.assertTrue(VerificationToken.objects.filter(user=user).exists())
+		
 
 	def test_register_via_invitation(self):
 		url = reverse('register')
@@ -85,4 +48,48 @@ class UserRegistrationTestCase(APITestCase):
 		self.assertTrue(user)
 		member = Membership.objects.filter(user=user).first()
 		self.assertEqual(member.team, inv.team)
-		
+
+	def test_verify_email(self):
+		url_reg = reverse('register')
+		responce = self.client.post(url_reg, {'email': self.email, 'password': self.password,})
+
+		user = CustomUser.objects.get(email=self.email)
+		self.assertFalse(user.email_verified)
+
+		verification = VerificationToken.objects.filter(user=user).first()
+		self.assertTrue(verification)
+
+		url_verify = reverse('verify_email')
+		responce = self.client.post(url_verify, {'code': verification.code.hex})
+		self.assertEqual(responce.status_code, 202)
+
+		user = CustomUser.objects.get(email=self.email)
+		self.assertTrue(user.email_verified)
+		self.assertFalse(VerificationToken.objects.filter(user=user).exists())
+
+
+class UserTestCase(APITestCase):
+	def setUp(self):
+		self.email = 'test@case.com'
+		self.password = 'P@ssw0rd'
+		self.user = CustomUser.objects.create_user(self.email, self.password)
+		Token.objects.create(user=self.user)
+
+	def tearDown(self):
+		Token.objects.filter(user=self.user).delete()  #for the case of logout test since token object can be deleted before
+		self.user.delete()
+
+	def test_login(self):
+		url_login = reverse('login')
+		responce = self.client.post(url_login, {'email': self.email, 'password': self.password})
+		token = Token.objects.get(user=self.user)
+		self.assertEquals(responce.status_code, 200)
+		self.assertEquals(token.key, responce.data['key'])
+
+	def test_logout(self):
+		url_logout = reverse('logout')
+		token = Token.objects.get(user=self.user)
+		self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+		responce = self.client.post(url_logout)
+		self.assertEqual(responce.status_code, 200)
+		self.assertFalse(Token.objects.filter(user=self.user).exists())
